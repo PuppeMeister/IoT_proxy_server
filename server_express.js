adf1547c8d599ec622df399f5ee5bdc19be2c4c3
Rin Nadia <luckylady9019@gmail.com>
2019-09-23 17:44:22 +0900

    Ready Post Data Retriever and gitignore

--- server_express.js
+++ server_express.js
@@ -41,7 +41,7 @@ try{
 	appConfig = require('./config/app_config.js');
 	express = require('express');
 	bodyParser = require('body-parser');
-	io = require('socket.io')(80);
+	io = require('socket.io')(19997);
 
 	//PORT
 	port = appConfig.port;
@@ -97,50 +97,23 @@ try{
 	logger.error("Application is failed to start. "+e);
 }
 
-function subcribeMqtt(){
 
-	var mqtt = require('mqtt')
-	//var mqttURL = "mqtt://223.195.37.170:1883/monitor/";
-	var mqttURL = 'mqtt://localhost:1883';
-	var clientMqtt  = mqtt.connect('mqtt://localhost:1883');
-	//var clientMqtt  = mqtt.connect('mqtt://test.mosquitto.org');
-	//var clientMqtt  = mqtt.connect(mqttURL);
-
-	clientMqtt.on('connect', function () {
-  	clientMqtt.subscribe('presence', function (err) {
-	
-		if (!err) {
-		//console.log("Successfully Subcribe! mqtt url ="+mqttURL);
-		try{
-			clientMqtt.publish('presence', 'Hello moto');
-			//setInterval(function(){clientMqtt.publish('presence','cricket');},3000);
-		}
-		catch(e){
-			console.log("Failed to publish "+e);
-		}
-    		}
-		  })
-		  
-		  clientMqtt.on('message', function (topic, message) {
-			// message is Buffer
-			/*if(message.toString().equals("")){
-				console.log("No Message!");
-			}*/
-			console.log("Get it!!!!!! "+message.toString())
-			clientMqtt.end()
-		  })
-	})
-}
 
 if(loadingApp){
 
 		const util = require('util');
 		var app = express();
 		app.use(bodyParser.urlencoded({ extended: false }));
-		app.use(bodyParser.json())
+		app.use(bodyParser.json());
 
-		var eventName = ["sendSolarData", "sendBatteryData","sendLoadData"];
-		var channelName = ["/solar", "/battery","/load"];
+		var solarEventEntityName = ["current", "voltage","power"];
+		var solarChannelName = ["/solarCurrent", "/solarVoltage","/solarPower"];
+
+		var batteryEventEntityName = ["level", "current","voltage", "power", "charging", "discharging"];
+		var batteryChannelName = ["/battLevel", "/battCurrent","/battVoltage","/battPower", "/battCharging", "/battDischarging"];
+
+		var loadEventEntityName = ["inCurrent", "inVoltage","inPower"];
+		var loadChannelName = ["/loadCurrent", "/loadVoltage","/loadPower"];
 
 		
 	
@@ -153,70 +126,41 @@ if(loadingApp){
 		});*/
 
 		var getRequestedData = {
-			doWork : function (req, res, activityName, eventName, channelName){	
+			doWork : function (req, res, activityName, eeName, channelName){	
 				
 				res = setReponseHeader(res);
 				res.statusCode = 200 ; 
 				res.send("Successful");
 				
-				var data = JSON.parse(req);
-				//var data = JSON.stringify(req.body);
-				console.log("Data here -->"+ data.body);
-				//console.log("Data here too -->"+ data.m2m:sgn);
+				for(i=0; i<eeName.length; i++){
 
-				//logger.debug("Retrieve "+activityName+ "Data Body = " +req.body.Body);
-				//logger.debug("Retrieve "+activityName+ "Data = " +req.bodyParser);
-
-				//console.log("Retrieve "+activityName+ "Data Header = " +req.headers);
-				//console.log("Retrieve here here "+activityName+ "Data = "+JSON.stringify(req));
-				
-
-				//console.log("---> "+util.inspect(req, false, null));
-				//var getData = getMobiusData(options);
-				
-				
-				/*var pusher = io.of(channelName).on('connection', function(socket){
-					console.log("Push to Client!");
-					socket.emit(eventName, data);	
-				});	*/
-
-
-				
-
-				/*getData.then(function(result) {
-				
-					var sentResult = {"finalResult" : result['m2m:cin']['con']};
-					res.send(sentResult);
-			
-				}, function(err) {
-				
-				res.statusCode = 404; 
-				res.send("{result : Failed Retriving}");
-				
-				console.log("Failed to Retrieve "+activityName+" "+err);
-				logger.error("Failed to Retrieve "+activityName+" "+err);
-				})*/
+					var sentData = req.body['m2m:sgn']['m2m:nev']['m2m:rep']['m2m:fcnt'][eeName[i]];
+					
+					console.log(activityName + "retrieved data = "+eeName[i]+" || "+sentData);
+					
+					var pusher = io.of(channelName[i]).on('connection', function(socket){
+						//console.log("Push to Client!");
+						socket.emit(eventName, sentData);	
+					});
+	
+				}
 				
 			}
 		};
 
 		app.post('/solar', (req, res) => {
 			
-			console.log("Ini Post toString -> "+req.toString());
-			console.log("Ini Post Body -> "+req.body);
-			//console.log("JSON? -> "+JSON.parse(req.body));
-	
-				//url: diURL['deviceType']
+			console.log("Incoming Solar Data = "+JSON.stringify(req.body));
+
 				var worker = getRequestedData;
-					//getData : getResultFromMobius(diURL['deviceType'], res, "Device Type")
-				
+			
 				
 				try{
 					console.log("Push Solar Data.");
-					worker.doWork(req, res, "Solar", eventName[0], channelName[0]);
+					worker.doWork(req, res, "Solar", solarEventName, solarchannelName);
 				
 				}catch(e){
-					console.log("Failed to Retrieve and to Push Solar Data"+e);
+					console.log("Failed to Retrieve and to Push Solar Data" +e);
 				}
 				finally{
 					delete worker;
@@ -228,15 +172,13 @@ if(loadingApp){
 		
 		app.post('/battery', (req, res) => {
 			
-			console.log("Ini Post toString -> "+req.toString());
-			console.log("Ini Post Body -> "+req.body);
-			//console.log("JSON? -> "+JSON.parse(req.body));
+			console.log("Incoming Battery Data = "+JSON.stringify(req.body));
 			
 			var worker = getRequestedData;
 			
 			try{
 				console.log("Push Battery Data.");
-				worker.doWork(req, res, "Battery", eventName[1], channelName[1]);
+				worker.doWork(req, res, "Battery", batteryEventEntityName, batteryChannelName);
 			
 			}catch(e){
 				console.log("Failed to Retrieve and to Push Battery Data"+e);
@@ -250,14 +192,13 @@ if(loadingApp){
 
 		app.post('/load', (req, res) => {
 			
-			console.log("Ini Post toString -> "+JSON.stringify(req.body));
-			//console.log("JSON? -> "+JSON.parse(req.body));
+			console.log("Incoming Load Data = "+JSON.stringify(req.body));
 
 			var worker = getRequestedData;
 			
 			try{
 				console.log("Push Load Data.");
-				worker.doWork(req, res, "Load", eventName[2], channelName[2]);
+				worker.doWork(req, res, "Load", loadEventEntityName, loadChannelName);
 			
 			}catch(e){
 				console.log("Failed to Retrieve and to Push Load Data"+e);
@@ -335,7 +276,8 @@ if(loadingApp){
 		//MQTT Part
 
 
-		app.listen(port, () => console.log('This app is listening on port 19998! and with POST listening'));
+		app.listen(port, () => console.log('This app is listening on port 19998! and with POST listening and web socket is on port 19997!'));
+		
 }
 
 
